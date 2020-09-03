4b548496880c1778cc227f97b959afd0f7d44e1d
John Orozco <john.o111@yahoo.com>
2020-09-03 01:46:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,14 @@
-def #fizzbuzz
+require_relative './spec_helper.rb'
 
-end
-  describe "fizzbuzz" do
-    it 'returns "Fizz" when the number is divisible by 3' do
+returns "Fizz" when the number is divisible by 3
       fizz_3 = fizzbuzz(3)
 
-      expect(fizz_3).to eq("Fizz")
+      expect(fizz_3) = "Fizz"
     end
-    it 'returns "Buzz" when the number is divisible by 5' do
+returns "Buzz" when the number is divisible by 5
       fizz_5 = fizzbuzz(5)
 
-      expect(fizz_5).to eq("Buzz")
+      expect(fizz_5) = ("Buzz")
     end
     it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
       fizz_15 = fizzbuzz(15)
